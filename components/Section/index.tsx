3ea3bd6b92571ed3f2d005ded3f6d1bfd2e5648b
James Michael <jomichael1994@gmail.com>
2026-01-29 00:25:15 +0000

    add Section component and colour map

---
+++ components/Section/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React from 'react';
+
+import colourMap from '@/constants/colours';
+
+import StretchText from '../StretchText';
+
+const Section = ({
+	title,
+	background = 'dark',
+	children,
+}: {
+	title?: string;
+	background?: keyof typeof colourMap;
+	children: React.ReactNode;
+}) => {
+	return (
+		<section className={`min-h-fit ${colourMap[background]}`}>
+			<div className="max-w-5xl mx-auto px-6 sm:px-8">
+				{title && (
+					<div className="h-24 sm:h-32 flex items-center">
+						<StretchText
+							text={title}
+							className="text-blue-400 uppercase font-georama leading-none font-extrabold text-3xl sm:text-5xl font-stretch-115%"
+							probability={0.15}
+						/>
+					</div>
+				)}
+				<div
+					className={`flex flex-col gap-8 pb-20 ${title ? '' : 'pt-20'}`}
+				>
+					{children}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Section;
---
+++ constants/colours.tsx
@@ -0,0 +1,5 @@
+export default {
+	dark: 'bg-slate-950',
+	light: 'bg-white',
+	accent: 'bg-blue-400',
+};
