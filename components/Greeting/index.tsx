ef4f55c0ba7ddea3dcacb47efe17e0c643edfacc
James Michael <jomichael1994@gmail.com>
2026-01-30 00:22:14 +0000

    add border radius to Greeting component

--- components/Greeting/index.tsx
+++ components/Greeting/index.tsx
@@ -8,7 +8,7 @@ const Greeting = () => {
 	return (
 		<Section background="light" className="py-20">
 			<div className="sm:my-36 flex justify-center items-center">
-				<div className="max-w-5xl bg-slate-950 text-white p-8 grid grid-rows-[auto,1fr] gap-6 select-text">
+				<div className="rounded-2xl max-w-5xl bg-slate-950 text-white p-8 grid grid-rows-[auto,1fr] gap-6 select-text">
 					<h1 className="font-georama leading-none font-stretch-150% font-extrabold text-blue-400 text-[2.8rem] sm:text-7xl lg:text-8xl">
 						Hello! 👋
 					</h1>
