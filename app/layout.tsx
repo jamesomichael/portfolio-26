a57554cc0221a4be751bff7dacc99a56d9f4d9f1
James Michael <jomichael1994@gmail.com>
2026-01-30 17:30:38 +0000

    update fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				<link
-					href="https://fonts.googleapis.com/css2?family=Georama:ital,wdth,wght@0,62.5..150,100..900;1,62.5..150,100..900&family=Unbounded:wght@200..900&family=Urbanist:ital,wght@0,100..900;1,100..900&family=Zalando+Sans:ital,wdth,wght@0,75..125,200..900;1,75..125,200..900&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Georama:ital,wdth,wght@0,62.5..150,100..900;1,62.5..150,100..900&family=Hubot+Sans:ital,wght@0,200..900;1,200..900&family=Unbounded:wght@200..900&family=Urbanist:ital,wght@0,100..900;1,100..900&family=Zalando+Sans:ital,wdth,wght@0,75..125,200..900;1,75..125,200..900&display=swap"
 					rel="stylesheet"
 				/>
 			</head>
