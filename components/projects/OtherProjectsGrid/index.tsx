96aa0249f7b2d5b394ebf146b8e4c5503748bcb9
James Michael <jomichael1994@gmail.com>
2026-01-30 23:23:20 +0000

    add components to display other project tiles

---
+++ components/projects/OtherProjectsGrid/index.tsx
@@ -0,0 +1,20 @@
+import ProjectTile from '../ProjectTile';
+
+import type { Project } from '@/types/projects';
+
+const OtherProjectsGrid = ({ projects }: { projects: [string, Project][] }) => {
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+			{projects.map(([key, project], idx) => (
+				<ProjectTile
+					key={key}
+					projectKey={key}
+					logo={project.projectTile.logo}
+					isHero={idx === 0}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default OtherProjectsGrid;
--- components/projects/Projects/index.tsx
+++ components/projects/Projects/index.tsx
@@ -2,6 +2,7 @@ import projects from '@/constants/projects';
 
 import Section from '../../Section';
 import FeaturedProjectsGrid from '../FeaturedProjectsGrid';
+import OtherProjectsGrid from '../OtherProjectsGrid';
 
 import type { Project } from '@/types/projects';
 
@@ -9,9 +10,15 @@ const Projects = () => {
 	const featuredProjects = Object.entries(projects).filter(
 		([, project]) => project.isFeatured,
 	) as [string, Project][];
+	const otherProjects = Object.entries(projects).filter(
+		([, project]) => !project.isFeatured,
+	) as [string, Project][];
 	return (
 		<Section background="dark" title="Projects">
-			<FeaturedProjectsGrid projects={featuredProjects} />
+			<div className="flex flex-col gap-3 lg:gap-4">
+				<FeaturedProjectsGrid projects={featuredProjects} />
+				<OtherProjectsGrid projects={otherProjects} />
+			</div>
 		</Section>
 	);
 };
