f61fdb954d3d72dbd94ed270b4b1a37dd202571b
James Michael <jomichael1994@gmail.com>
2026-02-02 22:32:49 +0000

    update project types

--- types/projects.ts
+++ types/projects.ts
@@ -1,6 +1,4 @@
-export interface Project {
-	name: string;
-	isFeatured?: boolean;
+interface ProjectBranding {
 	projectTile: {
 		backgroundGradient?: string;
 		logo: React.ReactNode;
@@ -8,4 +6,30 @@ export interface Project {
 	navbarDropdown: {
 		logo: React.ReactNode;
 	};
+	projectPage: {
+		backgroundGradient?: string;
+		logo: React.ReactNode;
+	};
+}
+
+interface ExternalLink {
+	type: 'website' | 'github' | 'storybook';
+	url: string;
+}
+
+interface ProjectMedia {
+	images?: {
+		desktop?: string[];
+		mobile?: string[];
+	};
+}
+
+export interface Project {
+	name: string;
+	isFeatured?: boolean;
+	branding: ProjectBranding;
+	headline: string;
+	description?: string;
+	externalLinks: ExternalLink[];
+	media?: ProjectMedia;
 }
