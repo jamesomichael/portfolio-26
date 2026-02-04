705b2dccd95bf3ebe37ff2df5cd8afd53d34422f
James Michael <jomichael1994@gmail.com>
2026-02-04 19:41:56 +0000

    adjust width of external links section

--- components/project/ExternalLinks/index.tsx
+++ components/project/ExternalLinks/index.tsx
@@ -5,7 +5,7 @@ import type { ProjectExternalLink } from '@/types/projects';
 const ExternalLinks = ({ links }: { links: ProjectExternalLink[] }) => {
 	return (
 		links.length > 0 && (
-			<div className="md:w-72 flex flex-col sm:grid sm:grid-cols-2 md:flex md:flex-col gap-3">
+			<div className="md:w-68 flex flex-col sm:grid sm:grid-cols-2 md:flex md:flex-col gap-3">
 				{links.map((link, idx) => (
 					<ExternalLink
 						key={idx}
