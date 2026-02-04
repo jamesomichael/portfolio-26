2a03a08f6271b5596ed516265b75b9125fb82dda
James Michael <jomichael1994@gmail.com>
2026-02-04 19:44:26 +0000

    adjust Skill component border radius

--- components/sections/skills/Skill/index.tsx
+++ components/sections/skills/Skill/index.tsx
@@ -11,7 +11,7 @@ const Skill = ({
 }) => {
 	return (
 		<div
-			className={`rounded-lg bg-linear-190 from-slate-950 ${gradientTo} mx-2.5 sm:mx-3.5 flex justify-center items-center gap-3 sm:gap-3.5 px-4 py-3.5 sm:px-5 sm:py-4 will-change-transform`}
+			className={`rounded-xl bg-linear-190 from-slate-950 ${gradientTo} mx-2.5 sm:mx-3.5 flex justify-center items-center gap-3 sm:gap-3.5 px-4 py-3.5 sm:px-5 sm:py-4 will-change-transform`}
 		>
 			<div className="w-8 h-8 sm:w-11 sm:h-11 flex justify-center items-center [&>svg]:w-full [&>svg]:h-full [&>img]:max-w-full [&>img]:max-h-full">
 				{icon}
