4481b71635d8b4f5a286f5edea1968c29c82716b
James Michael <jomichael1994@gmail.com>
2026-02-05 18:59:55 +0000

    update background opacity and z-index in TechTile component

--- components/project/TechTile/index.tsx
+++ components/project/TechTile/index.tsx
@@ -15,11 +15,11 @@ const TechTile = ({
 		<div
 			className={`relative rounded-xl sm:rounded-2xl bg-linear-to-b from-slate-900 ${gradientTo} h-44 w-40 sm:h-48 sm:w-44 grid grid-rows-[1fr_auto]`}
 		>
-			<div className="absolute inset-0 bg-linear-to-b from-transparent to-black/25 rounded-xl sm:rounded-2xl"></div>
-			<div className="relative z-50 flex justify-center items-center">
+			<div className="absolute inset-0 bg-linear-to-b from-transparent to-black/45 rounded-xl sm:rounded-2xl"></div>
+			<div className="relative z-40 flex justify-center items-center">
 				{icon}
 			</div>
-			<div className="relative z-50 h-11 px-2 pb-6 flex justify-center items-center">
+			<div className="relative z-40 h-11 px-2 pb-6 flex justify-center items-center">
 				<span className="line-clamp-2 font-georama text-sm sm:text-base leading-tight text-center font-bold font-stretch-125% text-slate-200">
 					{name}
 				</span>
