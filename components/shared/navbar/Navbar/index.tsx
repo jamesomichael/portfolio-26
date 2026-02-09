81c16b03fc7a9f114d55a8d329f52089943559d6
James Michael <jomichael1994@gmail.com>
2026-02-09 21:46:36 +0000

    update colours and icon in navbar

--- components/shared/navbar/Navbar/index.tsx
+++ components/shared/navbar/Navbar/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
-import { IoArrowBack } from 'react-icons/io5';
+import { FaArrowLeft } from 'react-icons/fa6';
 
 import ProjectsDropdown from '../ProjectsDropdown';
 import NavbarLogo from '../NavbarLogo';
@@ -13,15 +13,15 @@ const Navbar = () => {
 	const showNavbar = useNavbar();
 	return (
 		<div
-			className={`grid fixed top-0 left-0 right-0 z-50 border-b-2 border-slate-900/50 bg-slate-950 h-12 grid-cols-3 px-4 sm:px-6 transform transition-transform duration-300 ${
+			className={`grid fixed top-0 left-0 right-0 z-50 border-b border-slate-900/50 bg-slate-950 h-12 grid-cols-3 px-4 sm:px-6 transform transition-transform duration-300 ${
 				showNavbar ? 'translate-y-0' : '-translate-y-full'
 			}`}
 		>
 			<div className="flex items-center text-white">
-				<IoArrowBack
+				<FaArrowLeft
 					title="Back"
 					onClick={() => router.back()}
-					className="text-2xl text-slate-400 hover:text-white hover:cursor-pointer transition-colors duration-200"
+					className="h-5 w-5 text-slate-400 hover:text-white hover:cursor-pointer transition-colors duration-200"
 				/>
 			</div>
 			<div className="flex items-center justify-center">
--- components/shared/navbar/ProjectsDropdown/index.tsx
+++ components/shared/navbar/ProjectsDropdown/index.tsx
@@ -6,7 +6,7 @@ const ProjectsDropdown = () => {
 	const projectsArray = Object.entries(projects);
 	return (
 		<div className="relative group/dropdown">
-			<span className="font-urbanist text-slate-400 font-bold group-hover/dropdown:text-blue-400 transition-all duration-150 hover:cursor-pointer">
+			<span className="font-urbanist text-slate-400 font-bold group-hover/dropdown:text-white transition-all duration-150 hover:cursor-pointer">
 				Projects
 			</span>
 			<div className="transition-all duration-100 absolute top-9 right-0 rounded-xl z-50 bg-slate-950 p-2 w-80 flex flex-col gap-1.5 invisible group-hover/dropdown:visible">
